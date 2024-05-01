7a7080f7e700c278062bfa91a648b9df7857ec7b
ARAFAT <munshiarafathussain@gmail.com>
2024-05-01 06:33:02 +0600

    password min 1 char and notice for actions done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,21 @@ class UsersController < ApplicationController
   end
 
   def bulk_update
+    notice_message = ''
     User.transaction do
+      user_names = User.where(id: params[:user_ids]).pluck(:name)
       case params[:commit]
       when 'block'
         User.where(id: params[:user_ids]).update_all(status: 'blocked')
+        notice_message = "#{user_names.join(', ')} blocked successfully."
       when 'unblock'
         User.where(id: params[:user_ids]).update_all(status: 'active')
+        notice_message = "#{user_names.join(', ')} unblocked successfully."
       when 'delete'
         User.where(id: params[:user_ids]).destroy_all
+        notice_message = "#{user_names.join(', ')} deleted successfully."
       end
     end
-    redirect_to users_path, notice: 'Users updated.'
+    redirect_to users_path, notice: notice_message
   end
 end
